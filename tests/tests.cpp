b816f9c210564dd9dfec2ecafd7300582eda582d
thegavereguy <dallacia.enrico@protonmail.com>
2025-02-08 12:18:52 +0100

    cleaned up tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -17,85 +17,93 @@ const float expected[16] = {100,        86.71643,  74.04589,  63.181152,
                             119.290276, 143.68361, 171.68854, 200};
 
 TEST_CASE("Sequential solution - Test", "[seq]") {
-  Conditions conditions     = {1, 0.5, 0.1, 16, 30};
-  float* input              = new float[conditions.n_x];
+  Conditions conditions = {1, 0.5, 0.1, 16, 30};
+  float* input          = new float[conditions.n_x];
+  float* output         = new float[conditions.n_x];
+  initialize_array(input, conditions.n_x);
+  initialize_array(output, conditions.n_x);
+
   input[0]                  = 100;
   input[conditions.n_x - 1] = 200;
-  float* output             = new float[conditions.n_x];
 
   fmt::print("Sequential solution {}\n", conditions.alpha);
   sequential(conditions, input, output);
   for (int i = 0; i < conditions.n_x; i++) {
-    fmt::print("{}, ", output[i]);
     REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
   }
-  fmt::print("\n");
   delete[] input;
   delete[] output;
 }
 TEST_CASE("Parallel2 inner solution - Test", "[par2]") {
-  Conditions conditions     = {1, 0.5, 0.1, 16, 30};
-  float* input              = new float[conditions.n_x];
+  Conditions conditions = {1, 0.5, 0.1, 16, 30};
+  float* input          = new float[conditions.n_x];
+  float* output         = new float[conditions.n_x];
+  initialize_array(input, conditions.n_x);
+  initialize_array(output, conditions.n_x);
+
   input[0]                  = 100;
   input[conditions.n_x - 1] = 200;
-  float* output             = new float[conditions.n_x];
+
+  for (int i = 0; i < conditions.n_x; i++) {
+  }
 
   fmt::print("par2 solution{}\n", conditions.alpha);
   sequential(conditions, input, output);
   for (int i = 0; i < conditions.n_x; i++) {
-    fmt::print("{}, ", output[i]);
     REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
   }
-  fmt::print("\n");
   delete[] input;
   delete[] output;
 }
 TEST_CASE("Parallel4 inner solution - Test", "[par4]") {
-  Conditions conditions     = {1, 0.5, 0.1, 16, 30};
-  float* input              = new float[conditions.n_x];
+  Conditions conditions = {1, 0.5, 0.1, 16, 30};
+  float* input          = new float[conditions.n_x];
+  float* output         = new float[conditions.n_x];
+  initialize_array(input, conditions.n_x);
+  initialize_array(output, conditions.n_x);
   input[0]                  = 100;
   input[conditions.n_x - 1] = 200;
-  float* output             = new float[conditions.n_x];
+
   fmt::print("par4 solution\n");
   parallel4_inner(conditions, input, output);
   for (int i = 0; i < conditions.n_x; i++) {
-    fmt::print("{}, ", output[i]);
-    // REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
+    REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
   }
-  fmt::print("\n");
 
   delete[] input;
   delete[] output;
 }
 
 TEST_CASE("Parallel8 inner solution - Test", "[par8]") {
-  Conditions conditions     = {1, 0.5, 0.1, 16, 30};
-  float* input              = new float[conditions.n_x];
+  Conditions conditions = {1, 0.5, 0.1, 16, 30};
+  float* input          = new float[conditions.n_x];
+  float* output         = new float[conditions.n_x];
+  initialize_array(input, conditions.n_x);
+  initialize_array(output, conditions.n_x);
   input[0]                  = 100;
   input[conditions.n_x - 1] = 200;
-  float* output             = new float[conditions.n_x];
+
   parallel8_inner(conditions, input, output);
   for (int i = 0; i < conditions.n_x; i++) {
-    fmt::print("{}, ", output[i]);
-    // REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
+    REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
   }
-  fmt::print("\n");
   delete[] input;
   delete[] output;
 }
 
 TEST_CASE("Parallel2 outer solution - Test", "[par2_out]") {
-  Conditions conditions     = {1, 0.5, 0.1, 16, 30};
-  float* input              = new float[conditions.n_x];
+  Conditions conditions = {1, 0.5, 0.1, 16, 30};
+  float* input          = new float[conditions.n_x];
+  float* output         = new float[conditions.n_x];
+  initialize_array(input, conditions.n_x);
+  initialize_array(output, conditions.n_x);
   input[0]                  = 100;
   input[conditions.n_x - 1] = 200;
-  float* output             = new float[conditions.n_x];
+
   parallel2_outer(conditions, input, output);
   for (int i = 0; i < conditions.n_x; i++) {
-    fmt::print("{}, ", output[i]);
-    // REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
+    REQUIRE_THAT(output[i], Catch::Matchers::WithinAbs(expected[i], 0.001));
   }
-  fmt::print("\n");
   delete[] input;
   delete[] output;
 }
