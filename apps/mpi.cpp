709e9fe351a70d7b7a24fe381e4673fa7c105edb
thegavereguy <dallacia.enrico@protonmail.com>
2025-02-11 09:36:59 +0100

    updated mpi example

--- apps/mpi.cpp
+++ apps/mpi.cpp
@@ -1,18 +1,24 @@
+#include <fmt/base.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NX 16        // Total number of grid points
-#define NT 30        // Number of time steps
-#define ALPHA 0.5    // Diffusion coefficient
-#define DX 1.0 / NX  // Grid spacing
-#define DT 0.1 / NT  // Time step size
+#include <utility>
+
+const double L       = 1.0;  // Length of the rod
+const double alpha   = 0.5;  // Thermal diffusivity
+const double t_final = 0.1;  // Final time
+const int n_x        = 16;   // Number of spatial points
+const int n_t        = 30;   // Number of time steps
 
 void heat_equation(double *u, double *u_new, int local_nx, double lambda,
                    int rank, int size) {
+  float dt = t_final / (n_t);
+  float dx = L / (n_x - 1);
+
   MPI_Request request[4];  // Non-blocking send/receive requests
 
-  for (int t = 0; t < NT; t++) {
+  for (int t = 0; t < n_t; t++) {
     // Non-blocking communication: exchange ghost cells
     if (rank > 0) {
       MPI_Isend(&u[1], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD,
@@ -27,38 +33,55 @@ void heat_equation(double *u, double *u_new, int local_nx, double lambda,
                 &request[3]);  // Receive right neighbor
     }
 
+    fmt::print("rank = {}\n", rank);
     // Compute internal grid points (excluding ghost cells)
     for (int i = 2; i < local_nx; i++) {
-      u_new[i] = u[i] + lambda * (u[i - 1] - 2.0 * u[i] + u[i + 1]);
+      u_new[i] =
+          u[i] + lambda * (u[i - 1] - 2.0 * u[i] + u[i + 1]) * (dt / (dx * dx));
+      fmt::print("{} ", u_new[i]);
     }
+    fmt::print("\n");
 
+    // Wait for communication to complete
     if (rank > 0) MPI_Waitall(2, request, MPI_STATUSES_IGNORE);
     if (rank < size - 1) MPI_Waitall(2, &request[2], MPI_STATUSES_IGNORE);
 
     // Compute boundary points after receiving ghost cells
-    u_new[1] = u[1] + lambda * (u[0] - 2.0 * u[1] + u[2]);
+    u_new[1] = u[1] + lambda * (u[0] - 2.0 * u[1] + u[2]) * (dt / (dx * dx));
     u_new[local_nx] =
         u[local_nx] +
-        lambda * (u[local_nx - 1] - 2.0 * u[local_nx] + u[local_nx + 1]);
+        lambda * (u[local_nx - 1] - 2.0 * u[local_nx] + u[local_nx + 1]) *
+            (dt / (dx * dx));
+
+    if (rank == 0) {
+      u_new[1] = u[1];
+    } else if (rank == size - 1) {
+      u_new[local_nx] = u[local_nx];
+    }
+    // Update u
+    for (int i = 1; i <= local_nx; i++) {
+      u[i] = u_new[i];
+    }
 
     // Swap pointers
-    double *temp = u;
-    u            = u_new;
-    u_new        = temp;
+    std::swap(u, u_new);
   }
 }
 
 int main(int argc, char **argv) {
   int rank, size;
+  float dt = t_final / (n_t);
+  float dx = L / (n_x - 1);
+
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   // Local domain size per process (including ghost cells)
-  int local_nx = NX / size;
+  int local_nx = n_x / size;
   if (rank == size - 1)
     local_nx +=
-        NX % size;  // Last process gets extra points if NX is not divisible
+        n_x % size;  // Last process gets extra points if NX is not divisible
 
   // Allocate arrays (with extra ghost cells)
   double *u     = (double *)malloc((local_nx + 2) * sizeof(double));
@@ -67,13 +90,17 @@ int main(int argc, char **argv) {
   // Initialize local grid
   for (int i = 1; i <= local_nx; i++) {
     int global_i =
-        rank * (NX / size) + i;  // Convert local index to global index
-    u[i] = 0;
+        rank * (n_x / size) + i;  // Convert local index to global index
+    if (global_i == 1) {
+      u[i] = 100.0;
+    } else if (global_i == n_x) {
+      u[i] = 200.0;
+    } else {
+      u[i] = 0.0;
+    }
   }
-  u[0]            = 100;
-  u[local_nx - 1] = 200;
 
-  double lambda     = ALPHA * DT / (DX * DX);
+  double lambda     = alpha * dt / (dx * dx);
   double start_time = MPI_Wtime();
 
   heat_equation(u, u_new, local_nx, lambda, rank, size);
@@ -82,10 +109,10 @@ int main(int argc, char **argv) {
 
   // Gather results to rank 0
   if (rank == 0) {
-    double *global_u = (double *)malloc(NX * sizeof(double));
+    double *global_u = (double *)malloc(n_x * sizeof(double));
     MPI_Gather(&u[1], local_nx, MPI_DOUBLE, global_u, local_nx, MPI_DOUBLE, 0,
                MPI_COMM_WORLD);
-    for (int i = 0; i < NX; i++) {
+    for (int i = 0; i < n_x; i++) {
       printf("%f ", global_u[i]);
     }
     printf("Execution Time: %f seconds\n", end_time - start_time);
